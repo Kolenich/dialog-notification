db93c575d1d74bba2793bdcb9b91398bc6a79649
Kolenich <nick.zhigalin@gmail.com>
2021-02-11 23:26:55 +0300

    Changed imports

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export { default as DialogProvider } from './src/DialogProvider';
-export { default as useDialog } from './src/useDialog';
-export { default as withDialog } from './src/withDialog';
+export { default as DialogProvider } from 'DialogProvider';
+export { default as useDialog } from 'useDialog';
+export { default as withDialog } from 'withDialog';

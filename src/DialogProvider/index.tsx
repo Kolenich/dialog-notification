aac62efa0d6b417a73356763421dd67d68e2a03c
Kolenich <nick.zhigalin@gmail.com>
2021-02-16 20:58:57 +0300

    Converted provider component to class

--- src/DialogProvider/index.tsx
+++ src/DialogProvider/index.tsx
@@ -1,53 +1,46 @@
-import React, { createContext, FC, ReactNode, useCallback, useMemo, useState } from 'react';
+import React, { Component, createContext, ReactNode } from 'react';
 import DialogWindow from '../components/DialogWindow';
 import { DialogContextValue, DialogOptions, DialogProviderProps, DialogState } from './types';
 
 /** Main context */
 export const DialogContext = createContext<DialogContextValue>({} as DialogContextValue);
 
-/**
- * Provider component
- * @param {React.ReactNode} children - child components to be provided with context value
- * @param {DialogProviderProps} providerOptions - options from initializing DialogProvider
- * @returns {JSX.Element}
- * @constructor
- */
-const DialogProvider: FC<DialogProviderProps> = ({ children, ...providerOptions }) => {
-  const [dialogState, setDialogState] = useState<DialogState>({
-    open: false,
-    message: null,
-  });
-
-  const contextValue = useMemo(() => ({
-    /**
-     * Function for opening Dialog window
-     * @param {React.ReactNode} message - main content of the Dialog window
-     * @param {DialogOptions} options - additional options to Dialog window
-     */
-    openDialog: (message: ReactNode, options: DialogOptions = { variant: 'info' }) => setDialogState({
-      message,
-      options,
-      open: true,
-    }),
-    /** Function, that manually closes Dialog window */
-    closeDialog: () => setDialogState((oldState) => ({ ...oldState, open: false })),
-  }), []);
+/** Provider component */
+class DialogProvider extends Component<DialogProviderProps, DialogState> {
+  constructor(props: DialogProviderProps) {
+    super(props);
+    this.state = {
+      open: false,
+      message: null,
+    };
+  }
 
   /**
-   * Function for closing Dialog window
-   * @type {() => void}
+   * Function for opening Dialog window
+   * @param {React.ReactNode} message - main content of the Dialog window
+   * @param {DialogOptions} options - additional options to Dialog window
    */
-  const closeDialog = useCallback(() => setDialogState((oldDialogState) => ({
-    ...oldDialogState,
-    open: false,
-  })), []);
+  openDialog = (message: ReactNode, options?: DialogOptions) => this.setState({
+    message,
+    options,
+    open: true,
+  })
 
-  return (
-    <DialogContext.Provider value={contextValue}>
-      <DialogWindow {...providerOptions} {...dialogState} onClose={closeDialog}/>
-      {children}
-    </DialogContext.Provider>
-  );
-};
+  /** Function, that manually closes Dialog window */
+  closeDialog = () => this.setState({ open: false })
+
+  render() {
+    const { children, ...providerOptions } = this.props;
+
+    return (
+      <DialogContext.Provider
+        value={{ openDialog: this.openDialog, closeDialog: this.closeDialog }}
+      >
+        <DialogWindow {...providerOptions} {...this.state} onClose={this.closeDialog}/>
+        {children}
+      </DialogContext.Provider>
+    );
+  }
+}
 
 export default DialogProvider;
